fdf8c13197d398d91873ed0787557e685b697229
Ronan123321 <rdrismir@gmail.com>
2024-03-12 17:06:58 -0700

    Add project files.

---
+++ Connection.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Node.h"
+
+class Connection
+{
+	Node* frontNode, backNode;
+	double weight;
+
+};
---
+++ Node.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Connection.h"
+
+class Node
+{
+	double bias;
+
+	Connection* frontConnection, backConnection;
+
+
+};
